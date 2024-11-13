b80a3533d31f2b206192a343805e7c8345dc08d5
Prasanna Fuse <156182291+prasannafuse@users.noreply.github.com>
2024-11-14 03:32:11 +0530

    Update Q9_FCFS.java

--- Q9_FCFS.java
+++ Q9_FCFS.java
@@ -1,92 +1,63 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
-public class Q9_FCFS {
+public class FCFS {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of processes : ");
+        System.out.println("Enter number of processes: ");
         int processes = sc.nextInt();
 
-        int [] arrivalTimes = new int[processes];
-        int [] burstTimes = new int[processes];
-        for(int i = 0; i < processes; i++){
-            System.out.println("Enter arrival time for process P"+i+" : ");
+        int[] arrivalTimes = new int[processes];
+        int[] burstTimes = new int[processes];
+
+        for (int i = 0; i < processes; i++) {
+            System.out.println("Enter arrival time for process P" + i + ": ");
             arrivalTimes[i] = sc.nextInt();
-            System.out.println("Enter burst time for process P"+i+" : ");
+            System.out.println("Enter burst time for process P" + i + ": ");
             burstTimes[i] = sc.nextInt();
         }
-        // find min arrival times
-        int minArrivalTime = Integer.MAX_VALUE;
-        for (int arrivalTime : arrivalTimes) {
-            minArrivalTime = Math.min(minArrivalTime, arrivalTime);
+
+        // Array to store indices of processes sorted by arrival time
+        Integer[] indices = new Integer[processes];
+        for (int i = 0; i < processes; i++) indices[i] = i;
+
+        // Sort indices by arrival time to ensure FCFS order
+        Arrays.sort(indices, Comparator.comparingInt(i -> arrivalTimes[i]));
+
+        int[] finishTimes = new int[processes];
+        int[] turnaroundTimes = new int[processes];
+        int[] waitingTimes = new int[processes];
+
+        int time = 0;
+        for (int i = 0; i < processes; i++) {
+            int process = indices[i];
+
+            // If the process arrives after the current time, jump to its arrival time
+            if (time < arrivalTimes[process]) {
+                time = arrivalTimes[process];
+            }
+
+            // Calculate finish time
+            time += burstTimes[process];
+            finishTimes[process] = time;
+
+            // Calculate turnaround time and waiting time
+            turnaroundTimes[process] = finishTimes[process] - arrivalTimes[process];
+            waitingTimes[process] = turnaroundTimes[process] - burstTimes[process];
         }
 
-        Queue<Integer> readyQueue = new LinkedList<>();
-            for(int idx = 0; idx < arrivalTimes.length; idx++){
-                if(arrivalTimes[idx] == minArrivalTime){
-                    readyQueue.add(idx);
-                }
+        // Print the results
+        System.out.println("Process \t Arrival Time \t Burst Time \t Finish Time \t Turnaround Time \t Waiting Time");
+        for (int i = 0; i < processes; i++) {
+            System.out.printf("P%d\t\t %d\t\t %d\t\t %d\t\t\t %d\t\t %d\n",
+                    i, arrivalTimes[i], burstTimes[i], finishTimes[i], turnaroundTimes[i], waitingTimes[i]);
         }
-        System.out.println(readyQueue);
-
-        int [] finishTimes = new int[processes];
-        int [] turnaroundTimes = new int[processes]; // finish - arrival
-        int [] waitingTimes = new int[processes]; // turnaround - burst
-
-        int index = 0;
-        int finish = minArrivalTime;
-        while (!readyQueue.isEmpty()){
-            int process = readyQueue.poll();
-            // finish time
-            finish += burstTimes[process];
-            finishTimes[index] = finish;
-            // turnaround time
-            turnaroundTimes[index] = finishTimes[process] - arrivalTimes[process];
-            // waiting times
-            waitingTimes[index] = turnaroundTimes[process] - burstTimes[process];
-            // increment index
-            index++;
-        }
-
-        // pretty printing
-        System.out.println("Process \t\t Arrival Time \t\t Burst Time \t\t Finish Time \t\t Turnaround Time \t\t Waiting Time \t\t");
-        for(int i = 0; i < processes; i++){
-            System.out.printf("P%d\t\t\t\t\t%d\t\t\t\t\t%d\t\t\t\t\t%d\t\t\t\t\t%d\t\t\t\t\t\t%d\t\t\t\t\t" , i,arrivalTimes[i],burstTimes[i],finishTimes[i],turnaroundTimes[i],waitingTimes[i]);
-            System.out.println();
-        }
-
-        /*
-            How to run the program :
-            -> save file as Q9_FCFS.java   (save file in Desktop)
-            -> open terminal and type command cd Desktop
-            -> see whether there is Q9_FCFS.java file is present (use command ls for that)
-            -> if found , next command      javac Q9_FCFS.java
-            -> next command     java Q9_FCFS
-            -> Now your program is running , provide the input below.
-
-            Input :
-
-            No of Processes = 6
-
-            Process     Arrival Time       Burst Time
-            P0              0                   9
-            P1              1                   3
-            P2              1                   2
-            P3              1                   4
-            P4              2                   3
-            P5              3                   2
-
-            Output :
-
-            Process     Arrival Time       Burst Time       Finish Time        Turnaround Time      Waiting Time
-            P0              0                   9               9                   9                   0
-            P1              1                   3               12                  11                  8
-            P2              1                   2               14                  13                  11
-            P3              1                   4               18                  17                  13
-            P4              2                   3               21                  19                  16
-            P5              3                   2               23                  20                  18
-         */
     }
 }
+/*
+input : 4 0 2 1 2 5 3 6 4
+Process 	 Arrival Time 	 Burst Time 	 Finish Time 	 Turnaround Time 	 Waiting Time
+P0				 0				 2				 2					 2				 0
+P1				 1				 2				 4					 3				 1
+P2				 5				 3				 8					 3				 0
+P3				 6				 4				 12					 6				 2
+ */
